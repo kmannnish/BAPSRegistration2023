9ce957d215e098361bfaa0db202081c18a2e18f6
Manish Kumawat <62220845+kmannnish@users.noreply.github.com>
2023-02-09 05:39:40 +0530

    colorfull 1.1

--- script.js
+++ script.js
@@ -1,65 +1,69 @@
-$('.email').on("change keyup paste",
-    function () {
-        if ($(this).val()) {
-            $('.icon-paper-plane').addClass("next");
-        } else {
-            $('.icon-paper-plane').removeClass("next");
-        }
-    }
-);
+// $('.email').on("change keyup paste",
+//     function () {
+//         if ($(this).val()) {
+//             $('.icon-paper-plane').addClass("next");
+//         } else {
+//             $('.icon-paper-plane').removeClass("next");
+//         }
+//     }
+// );
 
-$('.next-button').hover(
-    function () {
-        $(this).css('cursor', 'pointer');
-    }
-);
+// $('.next-button').hover(
+//     function () {
+//         $(this).css('cursor', 'pointer');
+//     }
+// );
 
-$('.next-button.email').click(
-    function () {
-        console.log("Something");
-        $('.email-section').addClass("fold-up");
-        $('.mobile-section').removeClass("folded");
-    }
-);
+// $('.next-button.email').click(
+//     function () {
+//         console.log("Something");
+//         $('.email-section').addClass("fold-up");
+//         $('.mobile-section').removeClass("folded");
+//     }
+// );
 
-$('.mobile').on("change keyup paste",
-    function () {
-        if ($(this).val()) {
-            $('.icon-lock').addClass("next");
-        } else {
-            $('.icon-lock').removeClass("next");
-        }
-    }
-);
+// $('.mobile').on("change keyup paste",
+//     function () {
+//         if ($(this).val()) {
+//             $('.icon-lock').addClass("next");
+//         } else {
+//             $('.icon-lock').removeClass("next");
+//         }
+//     }
+// );
 
-$('.next-button').hover(
-    function () {
-        $(this).css('cursor', 'pointer');
-    }
-);
+// $('.next-button').hover(
+//     function () {
+//         $(this).css('cursor', 'pointer');
+//     }
+// );
 
-$('.next-button.mobile').click(
-    function () {
-        console.log("Something");
-        $('.mobile-section').addClass("fold-up");
-        $('.repeat-area-section').removeClass("folded");
-    }
-);
+// $('.next-button.mobile').click(
+//     function () {
+//         console.log("Something");
+//         $('.mobile-section').addClass("fold-up");
+//         $('.repeat-area-section').removeClass("folded");
+//     }
+// );
 
-$('.repeat-area').on("change keyup paste",
-    function () {
-        if ($(this).val()) {
-            $('.icon-repeat-lock').addClass("next");
-        } else {
-            $('.icon-repeat-lock').removeClass("next");
-        }
-    }
-);
+// $('.repeat-area').on("change keyup paste",
+//     function () {
+//         if ($(this).val()) {
+//             $('.icon-repeat-lock').addClass("next");
+//         } else {
+//             $('.icon-repeat-lock').removeClass("next");
+//         }
+//     }
+// );
 
-$('.next-button.repeat-area').click(
-    function () {
-        console.log("Something");
-        $('.repeat-area-section').addClass("fold-up");
-        $('.success').css("marginTop", 0);
-    }
-);
+// $('.next-button.repeat-area').click(
+//     function () {
+//         console.log("Something");
+//         $('.repeat-area-section').addClass("fold-up");
+//         $('.success').css("marginTop", 0);
+//     }
+// );
+
+// function showDiv(){
+//     document.getElementById('success').style.display = "block";
+// }
